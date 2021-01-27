e34b324b2d8c41959fe3824b6102c70007afe5b1
abzdev <abzdvlpmnt@gmail.com>
2021-01-26 18:52:35 -0800

    added chain.js and constrained bodies

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
+var chain, constrainedBox;
 var backgroundImg,platform;
 
 function preload() {
@@ -35,7 +37,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    constrainedBox = new Box(200,100,50,50);
 
+    chain = new Chain(bird.body,constrainedBox.body);
+    console.log(chain.bodyA);
 }
 
 function draw(){
@@ -61,4 +66,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    chain.display();
+    constrainedBox.display();
 }
